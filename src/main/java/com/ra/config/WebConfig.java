df3d54d7845fec174cf53c475c5cb9d4aa32b2e5
Tranhuy1612 <huytran1612@icloud.com>
2023-12-11 02:27:02 +0700

    Md5-ss2

---
+++ src/main/java/com/ra/config/WebConfig.java
@@ -0,0 +1,56 @@
+package com.ra.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+import java.util.Locale;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setForceEncoding(true);
+        filter.setEncoding("UTF-8");
+        return new Filter[]{filter};
+    }
+
+
+    @Bean
+    MessageSource messageSource (){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("lang");
+        messageSource.setDefaultEncoding("utf-8");
+        return messageSource;
+    }
+    @Bean
+    public LocaleResolver localeResolver (){
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("vi"));
+        return localeResolver;
+    }
+
+}
